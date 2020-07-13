4c0925cde413e80e0e03a1f6ccf88ecccc60c79a
Michael Young <mick.young1981@googlemail.com>
2020-07-13 09:29:04 +0100

    Added validation summary.

--- src/SFA.DAS.EmployerIncentives.Web/Controllers/ApplyController.cs
+++ src/SFA.DAS.EmployerIncentives.Web/Controllers/ApplyController.cs
@@ -21,7 +21,7 @@ namespace SFA.DAS.EmployerIncentives.Web.Controllers
         {
             if (!viewModel.HasTakenOnNewApprentices.HasValue)
             {
-                viewModel.AddError("HasTakenOnNewApprentices", "Select yes if you’ve taken on new apprentices that joined your payroll after 1 August 2020");
+                ModelState.AddModelError("HasTakenOnNewApprentices", "Select yes if you’ve taken on new apprentices that joined your payroll after 1 August 2020");
                 return View(viewModel);
             }
 
