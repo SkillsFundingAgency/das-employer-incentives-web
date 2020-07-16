096724c8a6e803715ac537c76937dd86a8455699
Dave Read <david.read@digital.education.gov.uk>
2020-07-16 15:59:24 +0100

    EI-99 upgrade web project to .net core 3.1

--- src/SFA.DAS.EmployerIncentives.Web/Startup.cs
+++ src/SFA.DAS.EmployerIncentives.Web/Startup.cs
@@ -7,14 +7,12 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Options;
-using Microsoft.IdentityModel.Logging;
 using SFA.DAS.Authorization.Context;
 using SFA.DAS.Authorization.Mvc.Extensions;
 using SFA.DAS.Configuration.AzureTableStorage;
 using SFA.DAS.EmployerIncentives.Web.Filters;
-using SFA.DAS.EmployerIncentives.Web.Infrastructure.Configuration;
 using SFA.DAS.Authorization.DependencyResolution.Microsoft;
+using Microsoft.IdentityModel.Logging;
 
 namespace SFA.DAS.EmployerIncentives.Web
 {
@@ -78,6 +76,7 @@ namespace SFA.DAS.EmployerIncentives.Web
                     {
                         options.Filters.Add(new GoogleAnalyticsFilter());
                         options.AddAuthorization();
+                        options.EnableEndpointRouting = false;
                     })
                 .AddControllersAsServices()
                 .SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
