e203f2e01058c9a6740a1df6b2d8ffd09eb3d5ed
Kevin <kevin@stephek.co.uk>
2020-07-31 10:02:45 +0100

    EI-167-fix-home-page-link

---
+++ src/SFA.DAS.EmployerIncentives.Web/ViewModels/Home/HomeViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace SFA.DAS.EmployerIncentives.Web.ViewModels.Home
+{
+    public class HomeViewModel
+    {
+        public string AccountId { get; }
+
+        public HomeViewModel(string accountId)
+        {
+            AccountId = accountId;
+        }
+    }
+}
