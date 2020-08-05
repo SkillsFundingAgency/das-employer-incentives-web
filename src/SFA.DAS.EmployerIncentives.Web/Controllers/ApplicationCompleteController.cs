f1a941c49d5f29722522b9d4242ac7744d6957bd
Dave Read <david.read@digital.education.gov.uk>
2020-08-05 14:07:51 +0100

    EI-91 add application complete controller

---
+++ src/SFA.DAS.EmployerIncentives.Web/Controllers/ApplicationCompleteController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SFA.DAS.EmployerIncentives.Web.Controllers
+{
+    public class ApplicationCompleteController : Controller
+    {
+        [HttpGet]
+        [Route("application-complete")]
+        public ViewResult Index()
+        {
+            return View();
+        }
+    }
+}
