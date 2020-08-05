52b517942f9e1655eaaf1166cfc242480134d16c
Michael Young <mick.young1981@googlemail.com>
2020-08-05 11:55:48 +0100

    Split out controller.

--- src/SFA.DAS.EmployerIncentives.Web.Tests/Controllers/ApplyController/QualificationQuestionTests/WhenALegalEntityIsSelected.cs
+++ src/SFA.DAS.EmployerIncentives.Web.Tests/Controllers/ApplyController/QualificationQuestionTests/WhenALegalEntityIsSelected.cs
@@ -2,17 +2,26 @@
 using FluentAssertions;
 using Microsoft.AspNetCore.Mvc;
 using NUnit.Framework;
+using SFA.DAS.EmployerIncentives.Web.Controllers;
 using SFA.DAS.EmployerIncentives.Web.ViewModels.Apply;
 
 namespace SFA.DAS.EmployerIncentives.Web.Tests.Controllers.ApplyController.QualificationQuestionTests
 {
     [TestFixture]
-    public class WhenALegalEntityIsSelected : ApplyControllerTestBase
+    public class WhenALegalEntityIsSelected
     {
+        private ApplyQualificationController _sut;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _sut = new Web.Controllers.ApplyQualificationController();
+        }
+
         [Test]
         public async Task Then_the_qualification_question_is_displayed()
         {
-            var result = await Sut.GetQualificationQuestion(new QualificationQuestionViewModel()) as ViewResult;
+            var result = await _sut.GetQualificationQuestion(new QualificationQuestionViewModel()) as ViewResult;
 
             result.Model.Should().BeOfType<QualificationQuestionViewModel>();
             result.ViewName.Should().Be("QualificationQuestion");
