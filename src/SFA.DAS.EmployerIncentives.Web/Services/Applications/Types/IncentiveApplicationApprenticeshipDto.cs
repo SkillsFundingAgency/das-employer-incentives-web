b3f3f6a6859ac96db74d7405a950f519604c38f9
Michael Young <mick.young1981@googlemail.com>
2020-08-07 14:37:07 +0100

    Added apprenticeship id

--- src/SFA.DAS.EmployerIncentives.Web/Services/Applications/Types/IncentiveApplicationApprenticeshipDto.cs
+++ src/SFA.DAS.EmployerIncentives.Web/Services/Applications/Types/IncentiveApplicationApprenticeshipDto.cs
@@ -2,6 +2,7 @@
 {
     public class IncentiveApplicationApprenticeshipDto
     {
+        public int ApprenticeshipId { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string CourseName { get; set; }
