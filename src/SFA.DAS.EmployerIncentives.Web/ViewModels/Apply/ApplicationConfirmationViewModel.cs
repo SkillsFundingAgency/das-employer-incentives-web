4a65bc9402b85868db408ff779c944be1ab822a8
Paul Graham <paul.graham@coprime.co.uk>
2020-08-07 15:18:12 +0100

    corected more merge issues

--- src/SFA.DAS.EmployerIncentives.Web/ViewModels/Apply/ApplicationConfirmationViewModel.cs
+++ src/SFA.DAS.EmployerIncentives.Web/ViewModels/Apply/ApplicationConfirmationViewModel.cs
@@ -25,7 +25,7 @@ namespace SFA.DAS.EmployerIncentives.Web.ViewModels.Apply
 
         public class ApplicationApprenticeship
         {
-            public long ApprenticeshipId { get; set; }
+            public string ApprenticeshipId { get; set; }
             public string DisplayName => $"{FirstName} {LastName}";
             public string FirstName { get; set; }
             public string LastName { get; set; }
