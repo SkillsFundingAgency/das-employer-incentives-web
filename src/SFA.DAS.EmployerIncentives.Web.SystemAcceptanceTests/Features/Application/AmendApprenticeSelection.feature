ae771b3bae6427b19d961cef414e961bc2a559ff
Paul Graham <paul.graham@coprime.co.uk>
2020-08-10 15:49:56 +0100

    tests for when we post the amended application

--- src/SFA.DAS.EmployerIncentives.Web.SystemAcceptanceTests/Features/Application/AmendApprenticeSelection.feature
+++ src/SFA.DAS.EmployerIncentives.Web.SystemAcceptanceTests/Features/Application/AmendApprenticeSelection.feature
@@ -3,6 +3,7 @@ Feature: AmendApprenticeSelection
 	As an employer applying for the new apprentice grant
 	If I choose to amend my previously selected eligible apprenticeships
 	I want to see the these apprentices checked 
+	And be able to change my previous selection
 	
 Scenario: An employer is changing the previously selected apprenticeships
 	Given there are eligible apprenticeships for the grant
@@ -16,3 +17,15 @@ Scenario: An employer is changing the previously selected apprenticeships, but i
 	When the employer returns to the select apprentices page
 	Then the employer can see the previous apprentices checked
 	But the additional apprentice is not on the list
+
+Scenario: An employer is changing the previously selected apprenticeships and submits the application without any apprentices selected
+	Given there are eligible apprenticeships for the grant
+	And a initial application has been created
+	When the employer updates application with no apprentices selected
+	Then the employer will receive an error
+
+Scenario: An employer is changing the previously selected apprenticeships and submits the application with apprentices selected
+	Given there are eligible apprenticeships for the grant
+	And a initial application has been created
+	When the employer updates application with apprentices selected
+	Then the update application request will be sent
--- src/SFA.DAS.EmployerIncentives.Web.SystemAcceptanceTests/Steps/Application/AmendApprenticeSelectionSteps.cs
+++ src/SFA.DAS.EmployerIncentives.Web.SystemAcceptanceTests/Steps/Application/AmendApprenticeSelectionSteps.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using FluentAssertions;
 using Newtonsoft.Json;
 using SFA.DAS.EmployerIncentives.Web.Services.LegalEntities.Types;
+using SFA.DAS.EmployerIncentives.Web.SystemAcceptanceTests.Extensions;
 using SFA.DAS.EmployerIncentives.Web.SystemAcceptanceTests.Services;
 using SFA.DAS.EmployerIncentives.Web.ViewModels.Apply.SelectApprenticeships;
 using SFA.DAS.HashingService;
@@ -85,6 +86,26 @@ namespace SFA.DAS.EmployerIncentives.Web.SystemAcceptanceTests.Steps.Application
                         .WithBody(JsonConvert.SerializeObject(_data.GetApplicationResponseWithFirstTwoApprenticesSelectedAndAnAdditionalApprentice, TestHelper.DefaultSerialiserSettings)));
         }
 
+        [When(@"the employer updates application with no apprentices selected")]
+        public async Task WhenTheEmployerUpdatesApplicationWithNoApprenticesSelected()
+        {
+            var hashedAccountId = _hashingService.HashValue(_data.AccountId);
+            var url = $"{hashedAccountId}/apply/select-new-apprentices/{_data.ApplicationId}";
+            var form = new SelectApprenticeshipsRequest();
+
+            _response = await _testContext.WebsiteClient.PostAsJsonAsync(url, form);
+        }
+
+        [When(@"the employer updates application with apprentices selected")]
+        public async Task WhenTheEmployerUpdatesApplicationWithApprenticesSelected()
+        {
+            var hashedAccountId = _hashingService.HashValue(_data.AccountId);
+            var url = $"{hashedAccountId}/apply/select-new-apprentices/{_data.ApplicationId}";
+            var form = new KeyValuePair<string, string>("SelectedApprenticeships", _hashingService.HashValue(1));
+
+            _response = await _testContext.WebsiteClient.PostAsJsonAsync(url, form);
+        }
+
         [When(@"the employer returns to the select apprentices page")]
         public async Task WhenTheEmployerReturnsToTheSelectApprenticesPage()
         {
@@ -95,7 +116,29 @@ namespace SFA.DAS.EmployerIncentives.Web.SystemAcceptanceTests.Steps.Application
             _response = await _testContext.WebsiteClient.GetAsync(url);
             _response.EnsureSuccessStatusCode();
         }
-        
+
+        [Then(@"the update application request will be sent")]
+        public void ThenTheUpdateApplicationRequestWillBeSent()
+        {
+
+
+        }
+
+        [Then(@"the employer will receive an error")]
+        public void ThenTheEmployerWillReceiveAnError()
+        {
+            var hashedAccountId = _hashingService.HashValue(_data.AccountId);
+            var viewResult = _testContext.ActionResult.LastViewResult;
+
+            viewResult.Should().NotBeNull();
+            var model = viewResult.Model as SelectApprenticeshipsViewModel;
+            model.Should().NotBeNull();
+            _response.Should().HaveTitle(model.Title);
+            _response.Should().HavePathAndQuery($"/{hashedAccountId}/apply/select-new-apprentices/{_data.ApplicationId}");
+            model.Should().HaveTitle(SelectApprenticeshipsViewModel.SelectApprenticeshipsMessage);
+            viewResult.Should().ContainError(model.FirstCheckboxId, model.Title);
+        }
+
         [Then(@"the employer can see the previous apprentices checked")]
         public void ThenTheEmployerCanSeeThePreviousApprenticesChecked()
         {
