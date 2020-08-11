233b9063a3f95765255c42d549f0bc99ac5c98c8
gdbarnes <gregdbarnes@gmail.com>
2020-08-11 14:15:52 +0100

    Add in vpv to GaData. Update views with correct vpv. Create partials.

--- src/SFA.DAS.EmployerIncentives.Web/Models/GoogleAnalyticsData.cs
+++ src/SFA.DAS.EmployerIncentives.Web/Models/GoogleAnalyticsData.cs
@@ -2,12 +2,13 @@
 
 namespace SFA.DAS.EmployerIncentives.Web.Models
 {
-    public class GoogleAnalyticsData
-    {
-        public string DataLoaded { get; set; } = "dataLoaded";
-        public string UserId { get; set; }
-        public string Acc { get; set; }
-        public string Ale { get; set; }
-        public IDictionary<string, string> Extras { get; set; } = new Dictionary<string, string>();
-    }
+  public class GoogleAnalyticsData
+  {
+    public string DataLoaded { get; set; } = "dataLoaded";
+    public string UserId { get; set; }
+    public string Acc { get; set; }
+    public string Ale { get; set; }
+    public string Vpv { get; set; }
+    public IDictionary<string, string> Extras { get; set; } = new Dictionary<string, string>();
+  }
 }
