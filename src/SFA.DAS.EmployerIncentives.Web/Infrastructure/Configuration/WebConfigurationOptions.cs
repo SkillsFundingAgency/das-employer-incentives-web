d3b577c6387b90e86bcfbe93cf624747941220fb
gdbarnes <gregdbarnes@gmail.com>
2020-08-13 11:50:46 +0100

    Add WebConfigurationOptions for zendesk

--- src/SFA.DAS.EmployerIncentives.Web/Infrastructure/Configuration/WebConfigurationOptions.cs
+++ src/SFA.DAS.EmployerIncentives.Web/Infrastructure/Configuration/WebConfigurationOptions.cs
@@ -3,11 +3,15 @@
     public class WebConfigurationOptions
     {
         public const string EmployerIncentivesWebConfiguration = "EmployerIncentivesWeb";
-
         public virtual string RedisCacheConnectionString { get; set; }
         public virtual string CommitmentsBaseUrl { get; set; }
         public virtual string AccountsBaseUrl { get; set; }
         public virtual string AllowedHashstringCharacters { get; set; }
         public virtual string Hashstring { get; set; }
+        public virtual string CdnBaseUrl { get; set; }
+        public virtual string ZenDeskHelpCentreUrl { get; set; }
+        public virtual string ZenDeskSnippetKey { get; set; }
+        public virtual string ZenDeskSectionId { get; set; }
+        public virtual string ZenDeskCobrowsingSnippetKey { get; set; }
     }
 }
