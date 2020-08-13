750718b75e523964c0b210dc9fb1009bdf12315c
gdbarnes <gregdbarnes@gmail.com>
2020-08-13 15:02:23 +0100

    Add zendesk config to main ViewModel

--- src/SFA.DAS.EmployerIncentives.Web/ViewModels/ViewModel.cs
+++ src/SFA.DAS.EmployerIncentives.Web/ViewModels/ViewModel.cs
@@ -1,12 +1,20 @@
-﻿namespace SFA.DAS.EmployerIncentives.Web.ViewModels
+﻿using SFA.DAS.EmployerIncentives.Web.Infrastructure.Configuration;
+
+namespace SFA.DAS.EmployerIncentives.Web.ViewModels
 {
     public class ViewModel
     {
-        public ViewModel(string title)
+        public ViewModel(string title, WebConfigurationOptions webConfiguration)
         {
             Title = title;
+            ZenDeskSnippetKey = webConfiguration.ZenDeskSnippetKey;
+            ZenDeskSectionId = webConfiguration.ZenDeskSectionId;
+            ZenDeskCobrowsingSnippetKey = webConfiguration.ZenDeskCobrowsingSnippetKey;
         }
 
         public string Title { get; }
+        public string ZenDeskSnippetKey { get; set; }
+        public string ZenDeskSectionId { get; set; }
+        public string ZenDeskCobrowsingSnippetKey { get; set; }
     }
 }
