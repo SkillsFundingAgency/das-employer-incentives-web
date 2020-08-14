ed255ab44260e911b718c247754c22aaf5d87ef8
Ruslan <ruslanss@gmail.com>
2020-08-14 09:01:38 +0100

    EI-208 Add Data Encryption service to be used to form secure URLs

---
+++ src/SFA.DAS.EmployerIncentives.Web.Tests/Services/DataEncodingServiceTests.cs
@@ -0,0 +1,54 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using SFA.DAS.EmployerIncentives.Web.Services;
+using System;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace SFA.DAS.EmployerIncentives.Web.Tests.Services
+{
+    public class DataEncodingServiceTests
+    {
+        private const string TestAesKey = "P5T1NjQ1xqo1FgFM8RG+Yg==";
+        private const string OriginalRaw = "DW5T8V|P0007983|Angus Sinclair|07365363563|ag@gmail.com|Harold Shipman|07356356353|hs@gmail.com|Fred West|07265625622|fs@gmail.com|Ian Brady|01367363783|ib@gmail.com|apps=53";
+
+        [Test]
+        public void CanEncryptStringDataPrefixedWithRandomIv()
+        {
+            // Arrange
+            var service1 = new DataEncryptionService(TestAesKey);
+
+            // Act
+            var encrypted = service1.Encrypt(OriginalRaw);
+
+            // Assert
+            Decrypt(encrypted).Should().Be(OriginalRaw);
+        }
+
+        private static string Decrypt(string ivAndData)
+        {
+            string decrypted;
+            var iv = Convert.FromBase64String(ivAndData.Substring(0, 24));
+            var cipher = Convert.FromBase64String(ivAndData.Substring(24));
+
+            using (var aes = new AesManaged())
+            {
+                aes.KeySize = 128;
+                aes.BlockSize = 128;
+                aes.Key = Convert.FromBase64String(TestAesKey);
+                aes.Mode = CipherMode.CBC;
+                aes.Padding = PaddingMode.PKCS7;
+                aes.IV = iv;
+                var decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+
+                using var ms = new MemoryStream(cipher);
+                var cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Read);
+                using var sr = new StreamReader(cs);
+
+                decrypted = sr.ReadToEnd();
+            }
+
+            return decrypted;
+        }
+    }
+}
---
+++ src/SFA.DAS.EmployerIncentives.Web/Services/DataEncryptionService.cs
@@ -0,0 +1,50 @@
+using System;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace SFA.DAS.EmployerIncentives.Web.Services
+{
+    public class DataEncryptionService
+    {
+        private readonly string _key;
+
+        public DataEncryptionService(string key)
+        {
+            _key = key;
+        }
+
+        public string Encrypt(string raw)
+        {
+            byte[] encrypted;
+            string iv;
+
+            using (var aes = GetAes())
+            {
+                iv = Convert.ToBase64String(aes.IV);
+                var encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+
+                using var ms = new MemoryStream();
+                var cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write);
+                using (var sw = new StreamWriter(cs)) sw.Write(raw);
+
+                encrypted = ms.ToArray();
+            }
+
+            return iv + Convert.ToBase64String(encrypted);
+        }
+
+        private AesManaged GetAes()
+        {
+#pragma warning disable S5542 // Encryption algorithms should be used with secure mode and padding scheme
+            return new AesManaged
+            {
+                KeySize = 128,
+                BlockSize = 128,
+                Key = Convert.FromBase64String(_key),
+                Mode = CipherMode.CBC, // TODO: Review this (see https://docs.microsoft.com/en-us/dotnet/standard/security/vulnerabilities-cbc-mode)
+                Padding = PaddingMode.PKCS7
+            };
+#pragma warning restore S5542 // Encryption algorithms should be used with secure mode and padding scheme
+        }
+    }
+}
