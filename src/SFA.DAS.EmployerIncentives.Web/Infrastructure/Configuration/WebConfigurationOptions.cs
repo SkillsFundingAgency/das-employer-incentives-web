bb4ac2e3e0586b59286cd24b39e80627537fcdfd
gdbarnes <gregdbarnes@gmail.com>
2020-08-14 09:17:05 +0100

    Remove unused WebConfigurationOptions

--- src/SFA.DAS.EmployerIncentives.Web/Infrastructure/Configuration/WebConfigurationOptions.cs
+++ src/SFA.DAS.EmployerIncentives.Web/Infrastructure/Configuration/WebConfigurationOptions.cs
@@ -8,8 +8,6 @@
         public virtual string AccountsBaseUrl { get; set; }
         public virtual string AllowedHashstringCharacters { get; set; }
         public virtual string Hashstring { get; set; }
-        public virtual string CdnBaseUrl { get; set; }
-        public virtual string ZenDeskHelpCentreUrl { get; set; }
         public virtual string ZenDeskSnippetKey { get; set; }
         public virtual string ZenDeskSectionId { get; set; }
         public virtual string ZenDeskCobrowsingSnippetKey { get; set; }
