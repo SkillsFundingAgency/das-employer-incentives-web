d5d63ceaa86cee9e7478b780c26b2e24e4bd606e
Ruslan <ruslanss@gmail.com>
2020-08-19 18:29:46 +0100

    Add more verbose logging to BankingDetailsService

--- src/SFA.DAS.EmployerIncentives.Web/Services/Applications/BankingDetailsService.cs
+++ src/SFA.DAS.EmployerIncentives.Web/Services/Applications/BankingDetailsService.cs
@@ -1,7 +1,8 @@
-﻿using SFA.DAS.EmployerIncentives.Web.Infrastructure;
+﻿using Microsoft.Extensions.Logging;
 using SFA.DAS.EmployerIncentives.Web.Services.Applications.Types;
 using System;
 using System.Net.Http;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 
@@ -10,17 +11,31 @@ namespace SFA.DAS.EmployerIncentives.Web.Services.Applications
     public class BankingDetailsService : IBankingDetailsService
     {
         private readonly HttpClient _client;
+        private readonly ILogger _logger;
 
-        public BankingDetailsService(HttpClient client)
+        public BankingDetailsService(HttpClient client, ILoggerFactory logger)
         {
             _client = client;
+            _logger = logger.CreateLogger(typeof(ILogger));
         }
 
         public async Task<BankingDetailsDto> GetBankingDetails(long accountId, Guid applicationId, string hashedAccountId)
         {
             var url = OuterApiRoutes.GetBankingDetailsUrl(accountId, applicationId, hashedAccountId);
 
-            var (_, data) = await _client.GetDataAsync<BankingDetailsDto>(url);
+            _logger.LogInformation("[BankingDetailsService] Call outer API: " + url);
+
+            using var response = await _client.GetAsync(url, HttpCompletionOption.ResponseHeadersRead);
+
+            _logger.LogInformation("[BankingDetailsService] Response status code: " + response.StatusCode);
+            if (response.IsSuccessStatusCode)
+            {
+                _logger.LogError("[BankingDetailsService] Error occurred: " + response.ReasonPhrase);
+            }
+
+            response.EnsureSuccessStatusCode();
+
+            var data = await JsonSerializer.DeserializeAsync<BankingDetailsDto>(await response.Content.ReadAsStreamAsync(), options: new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
 
             return data;
         }
