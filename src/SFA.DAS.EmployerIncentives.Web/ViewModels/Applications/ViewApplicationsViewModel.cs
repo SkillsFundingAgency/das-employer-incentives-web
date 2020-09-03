8f4a1dbb08d449f0d7f8175d88e43a279055146e
Dave Read <david.read@digital.education.gov.uk>
2020-09-03 13:30:51 +0100

    EI-301 create view for view applications

---
+++ src/SFA.DAS.EmployerIncentives.Web/ViewModels/Applications/ViewApplicationsViewModel.cs
@@ -0,0 +1,16 @@
+﻿
+using SFA.DAS.EmployerIncentives.Web.Models;
+using System.Collections.Generic;
+
+namespace SFA.DAS.EmployerIncentives.Web.ViewModels.Applications
+{
+    public class ViewApplicationsViewModel : ViewModel
+    {
+        public ViewApplicationsViewModel() : base ("Your hire a new apprentice payment applications")
+        {
+            
+        }
+
+        public IEnumerable<ApprenticeApplicationModel> Applications { get; set; }
+    }
+}
