d0a0d19ed590ca88f67923f6b449732a9a495ccd
Dave Read <david.read@digital.education.gov.uk>
2020-09-03 17:29:16 +0100

    EI-301 payments controller tests

---
+++ src/SFA.DAS.EmployerIncentives.Web.Tests/Controllers/PaymentsController/WhenPaymentApplicationsAccessed.cs
@@ -0,0 +1,68 @@
+﻿using AutoFixture;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.EmployerIncentives.Web.Models;
+using SFA.DAS.EmployerIncentives.Web.Services.Applications;
+using SFA.DAS.EmployerIncentives.Web.ViewModels.Applications;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.EmployerIncentives.Web.Tests.Controllers.PaymentsController
+{
+    public class WhenPaymentApplicationsAccessed
+    {
+        private Web.Controllers.PaymentsController _sut;
+        private Mock<IApplicationService> _service;
+        private Fixture _fixture;
+        private string _accountId;
+
+        [SetUp]
+        public void Arrange()
+        {
+            _service = new Mock<IApplicationService>();
+            _sut = new Web.Controllers.PaymentsController(_service.Object);
+            _fixture = new Fixture();
+            _accountId = _fixture.Create<string>();
+        }
+
+        [Test]
+        public async Task Then_the_view_contains_summary_for_submitted_applications()
+        {
+            // Arrange
+            var applications = new List<ApprenticeApplicationModel>();
+            applications.AddRange(_fixture.CreateMany<ApprenticeApplicationModel>(5));
+            foreach(var application in applications)
+            {
+                application.Status = "Submitted";
+            }
+            _service.Setup(x => x.GetList(_accountId)).ReturnsAsync(applications);
+
+            var result = await _sut.ListPayments(_accountId) as ViewResult;
+
+            var viewModel = result.Model as ViewApplicationsViewModel;
+            viewModel.Should().NotBeNull();
+            viewModel.Applications.Count().Should().Be(applications.Count());
+        }
+
+        [Test]
+        public async Task Then_the_view_contains_summary_for_only_submitted_applications()
+        {
+            // Arrange
+            var applications = new List<ApprenticeApplicationModel>();
+            applications.AddRange(_fixture.CreateMany<ApprenticeApplicationModel>(5));
+            applications[2].Status = "Submitted";
+            applications[4].Status = "Submitted";
+
+            _service.Setup(x => x.GetList(_accountId)).ReturnsAsync(applications);
+
+            var result = await _sut.ListPayments(_accountId) as ViewResult;
+
+            var viewModel = result.Model as ViewApplicationsViewModel;
+            viewModel.Should().NotBeNull();
+            viewModel.Applications.Count().Should().Be(applications.Count(x => x.Status == "Submitted"));
+        }
+    }
+}
--- src/SFA.DAS.EmployerIncentives.Web/Controllers/PaymentsController.cs
+++ src/SFA.DAS.EmployerIncentives.Web/Controllers/PaymentsController.cs
@@ -4,6 +4,7 @@ using SFA.DAS.EmployerIncentives.Web.Services.Applications;
 using SFA.DAS.EmployerIncentives.Web.ViewModels.Applications;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace SFA.DAS.EmployerIncentives.Web.Controllers
@@ -21,18 +22,9 @@ namespace SFA.DAS.EmployerIncentives.Web.Controllers
         [Route("payment-applications")]
         public async Task<ViewResult> ListPayments(string accountId)
         {
-            //var applications = await _applicationService.GetList(accountId);
-            //
-            // FOR TESTING
-            var applications = new List<ApprenticeApplicationModel>
-            {
-                new ApprenticeApplicationModel { ApplicationDate = new DateTime(2020,08,02), AccountId = 123, FirstName = "Fred", LastName = "Smith", LegalEntityName = "Freds Firm", TotalIncentiveAmount = 1500},
-                new ApprenticeApplicationModel { ApplicationDate = new DateTime(2020,08,02), AccountId = 123, FirstName = "Steve", LastName = "Jones", LegalEntityName = "Boots", TotalIncentiveAmount = 2000},
-                new ApprenticeApplicationModel { ApplicationDate = new DateTime(2020,08,02), AccountId = 123, FirstName = "Jane", LastName = "Jennings", LegalEntityName = "Company With A Long Name That Takes Up Space", TotalIncentiveAmount = 1500},
-            };
-            // FOR UI TESTING
+            var applications = await _applicationService.GetList(accountId);
 
-            var model = new ViewApplicationsViewModel { Applications = applications };
+            var model = new ViewApplicationsViewModel { Applications = applications.Where(x => x.Status == "Submitted") };
 
             return View(model);
         }
