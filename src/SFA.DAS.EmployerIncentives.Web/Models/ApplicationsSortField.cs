160eb950d8e05b71ed7dbf8b331607675f2d2641
Dave Read <david.read@digital.education.gov.uk>
2020-09-18 10:24:48 +0100

    EI-301 sort by last name when sorting by apprentice name

--- src/SFA.DAS.EmployerIncentives.Web/Models/ApplicationsSortField.cs
+++ src/SFA.DAS.EmployerIncentives.Web/Models/ApplicationsSortField.cs
@@ -4,7 +4,7 @@ namespace SFA.DAS.EmployerIncentives.Web.Models
     public static class ApplicationsSortField
     {
         public static string ApplicationDate = "ApplicationDate";
-        public static string ApprenticeName = "ApprenticeName";
+        public static string ApprenticeName = "LastName";
         public static string TotalIncentiveAmount = "TotalIncentiveAmount";
         public static string LegalEntityName = "LegalEntityName";
     }
