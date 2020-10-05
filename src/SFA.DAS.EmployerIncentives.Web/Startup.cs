706fe58973049201f19d539a2041b59b20749d24
gdbarnes <gregdbarnes@gmail.com>
2020-10-05 17:22:53 +0100

    Remove logging import

--- src/SFA.DAS.EmployerIncentives.Web/Startup.cs
+++ src/SFA.DAS.EmployerIncentives.Web/Startup.cs
@@ -5,7 +5,6 @@ using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.IdentityModel.Logging;
 using SFA.DAS.Authorization.Context;
 using SFA.DAS.Authorization.DependencyResolution.Microsoft;
