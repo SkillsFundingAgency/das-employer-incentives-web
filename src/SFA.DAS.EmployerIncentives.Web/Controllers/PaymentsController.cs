f8f138fb879bb58711e1dca29e80a354f7c624e7
Dave Read <david.read@digital.education.gov.uk>
2020-11-16 15:25:23 +0000

    EI-327 EI-389 select legal entity / skip if only one organisation

--- src/SFA.DAS.EmployerIncentives.Web/Controllers/PaymentsController.cs
+++ src/SFA.DAS.EmployerIncentives.Web/Controllers/PaymentsController.cs
@@ -5,6 +5,7 @@ using SFA.DAS.EmployerIncentives.Web.ViewModels.Applications;
 using SFA.DAS.EmployerIncentives.Web.Extensions;
 using System.Linq;
 using System.Threading.Tasks;
+using SFA.DAS.EmployerIncentives.Web.Services.LegalEntities;
 
 namespace SFA.DAS.EmployerIncentives.Web.Controllers
 {
@@ -12,14 +13,29 @@ namespace SFA.DAS.EmployerIncentives.Web.Controllers
     public class PaymentsController : Controller
     {
         private readonly IApplicationService _applicationService;
+        private readonly ILegalEntitiesService _legalEntitiesService;
 
-        public PaymentsController(IApplicationService applicationService)
+        public PaymentsController(IApplicationService applicationService, ILegalEntitiesService legalEntitiesService)
         {
             _applicationService = applicationService;
+            _legalEntitiesService = legalEntitiesService;
         }
 
         [Route("payment-applications")]
         public async Task<IActionResult> ListPayments(string accountId, string sortOrder, string sortField)
+        {
+            var legalEntities = await _legalEntitiesService.Get(accountId);
+
+            if (legalEntities.Count() > 1) 
+            {                
+                return RedirectToAction("ChooseOrganisation", new { accountId });
+            }
+
+            return RedirectToAction("ListPaymentsForLegalEntity", new { accountId, legalEntities.First().AccountLegalEntityId, sortOrder, sortField });
+        }
+
+        [Route("{accountLegalEntityId}/payment-applications")]
+        public async Task<IActionResult> ListPaymentsForLegalEntity(string accountId, string accountLegalEntityId, string sortOrder, string sortField)
         {
             if (string.IsNullOrWhiteSpace(sortOrder))
             {
@@ -30,12 +46,12 @@ namespace SFA.DAS.EmployerIncentives.Web.Controllers
                 sortField = ApplicationsSortField.ApprenticeName;
             }
 
-            var applications = await _applicationService.GetList(accountId);
+            var applications = await _applicationService.GetList(accountId, accountLegalEntityId);
             var submittedApplications = applications.Where(x => x.Status == "Submitted").AsQueryable();
 
             if (!submittedApplications.Any())
             {
-                return RedirectToAction("NoApplications");
+                return RedirectToAction("NoApplications", new { accountId, accountLegalEntityId });
             }
 
             submittedApplications = SortApplications(sortOrder, sortField, submittedApplications);
@@ -47,13 +63,41 @@ namespace SFA.DAS.EmployerIncentives.Web.Controllers
             };
             model.SetSortOrder(sortField, sortOrder);
 
-            return View(model);
+            return View("ListPayments", model);
         }
 
-        [Route("no-applications")]
+        [Route("{accountLegalEntityId}/no-applications")]
         public ViewResult NoApplications()
         {
             var model = new NoApplicationsViewModel();
+            return View(model);
+        }
+
+        [Route("choose-organisation")]
+        [HttpGet]
+        public async Task<IActionResult> ChooseOrganisation(string accountId)
+        {
+            var legalEntities = await _legalEntitiesService.Get(accountId);
+            var model = new ChooseOrganisationViewModel { AccountId = accountId, LegalEntities = legalEntities };
+            return View(model);
+        }
+
+        [Route("choose-organisation")]
+        [HttpPost]
+        public async Task<IActionResult> ChooseOrganisation(ChooseOrganisationViewModel model)
+        {
+            if (!string.IsNullOrEmpty(model.Selected))
+            {
+                return RedirectToAction("ListPaymentsForLegalEntity", new { model.AccountId, accountLegalEntityId = model.Selected });
+            }
+
+            model.LegalEntities = await _legalEntitiesService.Get(model.AccountId);
+
+            if (string.IsNullOrEmpty(model.Selected))
+            {
+                ModelState.AddModelError(model.LegalEntities.Any() ? model.LegalEntities.First().AccountLegalEntityId : "LegalEntityNotSelected", model.LegalEntityNotSelectedMessage);
+            }
+
             return View(model);
         }
 
