f909cddc9db387bc033b13bb5b9be97cb43dd8e4
Dave Read <david.read@digital.education.gov.uk>
2021-01-28 14:08:11 +0000

    EI-481 basic hub page view with links

--- src/SFA.DAS.EmployerIncentives.Web/Controllers/HubController.cs
+++ src/SFA.DAS.EmployerIncentives.Web/Controllers/HubController.cs
@@ -1,14 +1,32 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SFA.DAS.EmployerIncentives.Web.Services.LegalEntities;
+using SFA.DAS.EmployerIncentives.Web.ViewModels;
 using System.Threading.Tasks;
 
 namespace SFA.DAS.EmployerIncentives.Web.Controllers
 {
     public class HubController : Controller
     {
+        private ILegalEntitiesService _legalEntitiesService;
+
+        public HubController(ILegalEntitiesService legalEntitiesService)
+        {
+            _legalEntitiesService = legalEntitiesService;
+        }
+
         [Route("{accountId}/{accountLegalEntityId}/hire-new-apprentice-payment")]
         public async Task<IActionResult> Index(string accountId, string accountLegalEntityId)
         {
-            return View();
+            var legalEntity = await _legalEntitiesService.Get(accountId, accountLegalEntityId);
+
+            var model = new HubPageViewModel
+            {
+                AccountId = accountId,
+                AccountLegalEntityId = accountLegalEntityId,
+                OrganisationName = legalEntity?.Name
+            };
+
+            return View(model);
         }
     }
 }
---
+++ src/SFA.DAS.EmployerIncentives.Web/ViewModels/HubPageViewModel.cs
@@ -0,0 +1,10 @@
+﻿
+namespace SFA.DAS.EmployerIncentives.Web.ViewModels
+{
+    public class HubPageViewModel
+    {
+        public string AccountId { get; set; }
+        public string AccountLegalEntityId { get; set; }
+        public string OrganisationName { get; set; }
+    }
+}
