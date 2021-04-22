0fdbaa50dbdba018d4b3312ae8503d76b812137c
Kevin <kevin@stephek.co.uk>
2021-04-22 18:43:35 +0100

    EI-1026-display-employer-agreement-required-banner-on-EI-hub

--- src/SFA.DAS.EmployerIncentives.Web.SystemAcceptanceTests/Features/Hub/HomePage.feature
+++ src/SFA.DAS.EmployerIncentives.Web.SystemAcceptanceTests/Features/Hub/HomePage.feature
@@ -13,3 +13,13 @@ Scenario: An employer with a multiple organisations visits the home page
 	Given an employer with a multiple organisations wants to view the home page
 	When the employer visits the home page
 	Then the employer is informed that they need to select an organisation
+
+Scenario: An employer with an application on a later agreement that needs signing is prompted to sign the agreement
+	Given an employer with a later agreement version that needs signing
+	When the employer is on the hub page
+	Then the accept new employer agreement call to action is shown
+
+Scenario: An employer with an application that is on the signed version is not prompted to sign the agreement
+	Given an employer with an agreement version that has been signed
+	When the employer is on the hub page
+	Then the accept new employer agreement call to action is not shown
--- src/SFA.DAS.EmployerIncentives.Web.SystemAcceptanceTests/Steps/Hub/HomePageSteps.cs
+++ src/SFA.DAS.EmployerIncentives.Web.SystemAcceptanceTests/Steps/Hub/HomePageSteps.cs
@@ -1,12 +1,15 @@
 ﻿using FluentAssertions;
 using Newtonsoft.Json;
 using SFA.DAS.EmployerIncentives.Web.Infrastructure;
+using SFA.DAS.EmployerIncentives.Web.Models;
 using SFA.DAS.EmployerIncentives.Web.SystemAcceptanceTests.Extensions;
 using SFA.DAS.EmployerIncentives.Web.SystemAcceptanceTests.Services;
 using SFA.DAS.EmployerIncentives.Web.ViewModels;
 using SFA.DAS.EmployerIncentives.Web.ViewModels.Apply;
 using SFA.DAS.EmployerIncentives.Web.ViewModels.Home;
 using SFA.DAS.EmployerIncentives.Web.ViewModels.Hub;
+using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -30,8 +33,7 @@ namespace SFA.DAS.EmployerIncentives.Web.SystemAcceptanceTests.Steps.Hub
             _testDataStore = _testContext.TestDataStore;
         }
 
-        [Given(@"an employer with a single organisation wants to apply for a grant")]
-        [Given(@"an employer with a selected organisation wants to apply for a grant")]
+        [Given(@"an employer with a single organisation wants to view the home page")]
         public void GivenAnEmployerWithASingleOrganisationWantsToApplyForAGrant()
         {
             var testdata = new TestData.Account.WithSingleLegalEntityWithEligibleApprenticeships();
@@ -113,6 +115,18 @@ namespace SFA.DAS.EmployerIncentives.Web.SystemAcceptanceTests.Steps.Hub
                  .WithStatusCode(HttpStatusCode.OK));
         }
 
+        [Given(@"an employer with a later agreement version that needs signing")]
+        public void GivenAnEmployerWithALaterAgreementVersionThatNeedsSigning()
+        {
+            GivenAnEmployerWithALaterAgreementVersionThatIs(true);
+        }
+
+        [Given(@"an employer with an agreement version that has been signed")]
+        public void GivenAnEmployerWithALaterAgreementVersionThatHasBeenSigned()
+        {
+            GivenAnEmployerWithALaterAgreementVersionThatIs(false);
+        }
+
         [When(@"the employer visits the home page")]
         public async Task WhenTheEmployerAppliesForTheGrant()
         {
@@ -121,6 +135,25 @@ namespace SFA.DAS.EmployerIncentives.Web.SystemAcceptanceTests.Steps.Hub
             var request = new HttpRequestMessage(
                 HttpMethod.Get,
                 $"{hashedAccountId}/apply");
+
+            var response = await _testContext.WebsiteClient.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+
+            _testContext.TestDataStore.GetOrCreate("Response", onCreate: () =>
+            {
+                return response;
+            });
+        }
+
+        [When(@"the employer is on the hub page")]
+        public async Task WhenTheEmployerIsOnTheHubPage()
+        {
+            var hashedAccountId = _testDataStore.Get<string>("HashedAccountId");
+            var hashedAccountLegalEntityId = _testDataStore.Get<string>("HashedAccountLegalEntityId");
+
+            var request = new HttpRequestMessage(
+                HttpMethod.Get,
+                $"/{hashedAccountId}/{hashedAccountLegalEntityId}/hire-new-apprentice-payment");
 
             var response = await _testContext.WebsiteClient.SendAsync(request);
             response.EnsureSuccessStatusCode();
@@ -203,7 +236,87 @@ namespace SFA.DAS.EmployerIncentives.Web.SystemAcceptanceTests.Steps.Hub
 
             response.Should().HaveTitle("Hire a new apprentice payment");
             response.Should().HavePathAndQuery($"/{hashedAccountId}/{hashedAccountLegalEntityId}/hire-new-apprentice-payment");
+        }
 
+        [Then(@"the accept new employer agreement call to action is shown")]
+        public void ThenTheShowAcceptNewEmployerAgreementIsShown()
+        {
+            var hashedAccountId = _testDataStore.Get<string>("HashedAccountId");
+            var response = _testDataStore.Get<HttpResponseMessage>("Response");
+            response.Should().HaveLink("[data-linktype='view-agreement']", $"{_testContext.ExternalLinksOptions.ManageApprenticeshipSiteUrl}/accounts/{hashedAccountId}/agreements");
+        }
+
+        [Then(@"the accept new employer agreement call to action is not shown")]
+        public void ThenTheShowAcceptNewEmployerAgreementIsNotShown()
+        {
+            var hashedAccountId = _testDataStore.Get<string>("HashedAccountId");
+            var response = _testContext.TestDataStore.Get<HttpResponseMessage>("Response");
+            response.Should().NotHaveLink("[data-linktype='view-agreement']", $"{_testContext.ExternalLinksOptions.ManageApprenticeshipSiteUrl}/accounts/{hashedAccountId}/agreements");
+        }
+
+        private void GivenAnEmployerWithALaterAgreementVersionThatIs(bool isSigned)
+        {
+            var testdata = new TestData.Account.WithSingleLegalEntityWithEligibleApprenticeships();
+            _testDataStore.Add("HashedAccountId", testdata.HashedAccountId);
+            _testContext.AddOrReplaceClaim(EmployerClaimTypes.Account, testdata.HashedAccountId);
+            _testDataStore.Add("HashedAccountLegalEntityId", testdata.HashedAccountLegalEntityId);
+
+            _testContext.EmployerIncentivesApi.MockServer
+           .Given(
+                   Request
+                   .Create()
+                   .WithPath($"/accounts/{testdata.AccountId}/legalentities")
+                   .UsingGet()
+                   )
+               .RespondWith(
+             Response.Create()
+               .WithStatusCode(HttpStatusCode.OK)
+               .WithBody(JsonConvert.SerializeObject(testdata.LegalEntities, TestHelper.DefaultSerialiserSettings)));
+
+            _testContext.EmployerIncentivesApi.MockServer
+             .Given(
+                     Request
+                     .Create()
+                     .WithPath($"/apprenticeships")
+                     .WithParam("accountid", testdata.AccountId.ToString())
+                     .WithParam("accountlegalentityid", testdata.LegalEntities.First().AccountLegalEntityId.ToString())
+                     .UsingGet()
+                     )
+                 .RespondWith(
+             Response.Create()
+                 .WithBody(JsonConvert.SerializeObject(testdata.Apprentices, TestHelper.DefaultSerialiserSettings))
+                 .WithStatusCode(HttpStatusCode.OK));
+
+            _testContext.EmployerIncentivesApi.MockServer
+              .Given(
+                      Request
+                      .Create()
+                      .WithPath($"/accounts/{testdata.AccountId}/legalentities/{testdata.LegalEntities.First().AccountLegalEntityId}")
+                      .UsingGet()
+                      )
+                  .RespondWith(
+              Response.Create()
+                  .WithBody(JsonConvert.SerializeObject(testdata.LegalEntities.First()))
+                  .WithStatusCode(HttpStatusCode.OK));
+
+            var applications = new List<ApprenticeApplicationModel>
+            {
+               new ApprenticeApplicationModel { AccountId = testdata.AccountId, ApplicationDate = new DateTime(2020, 09, 01), FirstName = "Jane", LastName = "Doe", TotalIncentiveAmount = 1500m, LegalEntityName = $"Organisation {testdata.LegalEntities.First().AccountLegalEntityId}", ULN = 900004567, CourseName = "Accounting", FirstPaymentStatus = new PaymentStatusModel { RequiresNewEmployerAgreement = isSigned } }
+            };
+
+            var getApplications = new GetApplicationsModel { ApprenticeApplications = applications, FirstSubmittedApplicationId = Guid.NewGuid() };
+
+            _testContext.EmployerIncentivesApi.MockServer
+                .Given(
+                    Request
+                        .Create()
+                        .WithPath($"/accounts/{testdata.AccountId}/legalentity/{testdata.LegalEntities.First().AccountLegalEntityId}/applications")
+                        .UsingGet()
+                )
+                .RespondWith(
+                    Response.Create()
+                        .WithStatusCode(HttpStatusCode.OK)
+                        .WithBody(JsonConvert.SerializeObject(getApplications)));
         }
     }
 }
