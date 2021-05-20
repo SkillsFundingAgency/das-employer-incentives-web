a0fecf18c31b8c7f9a5554e1cc519c26ae38d971
Kevin <kevin@stephek.co.uk>
2021-05-20 11:35:35 +0100

    EI-1157-prevent-phase-1-applications-after-cut-off-date

--- src/SFA.DAS.EmployerIncentives.Web/Filters/ApplicationShutterFilterAttribute.cs
+++ src/SFA.DAS.EmployerIncentives.Web/Filters/ApplicationShutterFilterAttribute.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Configuration;
 using SFA.DAS.EmployerIncentives.Web.Infrastructure.Configuration;
 using System;
 
@@ -7,17 +8,26 @@ namespace SFA.DAS.EmployerIncentives.Web.Filters
 {
     public class ApplicationShutterFilterAttribute : ActionFilterAttribute
     {
-        private readonly DateTime _applyFromDate;
-        public ApplicationShutterFilterAttribute(WebConfigurationOptions options)
+        private readonly IConfiguration _configuration;
+        private DateTime _applyFromDate;
+        private bool _isInitialised = false;
+
+        public ApplicationShutterFilterAttribute(IConfiguration configuration)
         {
-            DateTime.TryParse(options.ApplicationShutterPageDate, out _applyFromDate);
+            _configuration = configuration;
         }
+
         public override void OnActionExecuting(ActionExecutingContext context)
         {
             if (!(context.Controller is Controller controller))
             {
                 return;
             }
+
+            if (!_isInitialised)
+            {
+                Initialise();
+            }            
 
             if (_applyFromDate != DateTime.MinValue && DateTime.Today >= _applyFromDate.Date)
             {
@@ -31,6 +41,14 @@ namespace SFA.DAS.EmployerIncentives.Web.Filters
             }
 
              base.OnActionExecuting(context);
+        }
+
+        private void Initialise()
+        {
+            var webOptions = new WebConfigurationOptions();
+            _configuration.GetSection(WebConfigurationOptions.EmployerIncentivesWebConfiguration).Bind(webOptions);
+            DateTime.TryParse(webOptions.ApplicationShutterPageDate, out _applyFromDate);
+            _isInitialised = true;
         }
     }
 }
