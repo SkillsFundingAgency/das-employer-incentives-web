14a5fe41559dd92eedd82481335ddf8d6e882744
Michael Young <mick.young1981@googlemail.com>
2022-02-24 11:06:32 +0000

    Standardise project names

--- src/SFA.DAS.EmployerIncentives.Web.SystemAcceptanceTests/Features/Hub/EmployerIncentivesHub.feature.cs
+++ src/SFA.DAS.EmployerIncentives.Web.SystemAcceptanceTests/Features/Hub/EmployerIncentivesHub.feature.cs
@@ -10,7 +10,7 @@
 // ------------------------------------------------------------------------------
 #region Designer generated code
 #pragma warning disable
-namespace SFA.DAS.EmployerIncentives.Web.SystemAcceptanceTests.Features.Hub
+namespace SFA.DAS.EmployerIncentives.Web.AcceptanceTests.Features.Hub
 {
     using TechTalk.SpecFlow;
     using System;
