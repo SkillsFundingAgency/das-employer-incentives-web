3f1bd033e2eae5307e26fb0e88043691c0c7bac2
joshreid22 <joshreid295@gmail.com>
2022-05-17 15:57:37 +0100

    EI-1725 removed links

--- src/SFA.DAS.EmployerIncentives.Web/ViewModels/System/ApplicationsClosedModel.cs
+++ src/SFA.DAS.EmployerIncentives.Web/ViewModels/System/ApplicationsClosedModel.cs
@@ -2,7 +2,6 @@
 {
     public class ApplicationsClosedModel
     {
-        public string Title => "Applications open on 11 January 2022";
         public string Title => "Applications for the hire a new apprentice incentive payment closed on 20 May 2022.";
         public string AccountHomeUrl { get; set; }
         public string AccountId { get; set; }
